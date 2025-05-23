58ba4bfd6ccc9ea58e2b5c760a50ba9714231872
Ron Georgia <rong1611@gmail.com>
2025-05-22 20:38:27 -0400

    added index.html

--- docs/conf.py
+++ docs/conf.py
@@ -29,13 +29,33 @@ pygments_style = "sphinx"
 pygments_dark_style = "monokai"
 
 # Consider using alabaster
-html_theme = "alabaster"
-
+html_theme = "furo"
+# html_theme = "alabaster"
+"""
 html_theme_options = {
     "description": "",
     "page_width": "95%",
     "body_max_width": "auto",
     "fixed_sidebar": "false",
     "sidebar_collapse": "true",
+}
+"""
+html_theme_options = {
+    "light_logo": "logo-light-mode.png",
+    "dark_logo": "logo-dark-mode.png",
+    "logo_only": False,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": False,
+    "vcs_pageview_mode": "github",
+    "style_nav_header_background": "white",
+    "flyout_display": "hidden",
+    "version_selector": True,
+    "language_selector": True,
+    # Toc options
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False
 }
 
